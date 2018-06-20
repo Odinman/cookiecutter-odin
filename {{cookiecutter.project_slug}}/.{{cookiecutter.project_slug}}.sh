6dd72fc308f68f70449ac296e174ae9efaf7364a
Odin Lee <odinmanlee@gmail.com>
2018-06-20 15:13:40 +0800

    update

--- {{cookiecutter.project_slug}}/.{{cookiecutter.project_slug}}.sh
+++ {{cookiecutter.project_slug}}/.{{cookiecutter.project_slug}}.sh
@@ -1,18 +1,28 @@
-#! /bin/sh
+#!/bin/bash
 #
 # .{{cookiecutter.project_slug}}.sh
 # Copyright (C) 2018 Odin <Odin@Odin-Pro.local>
 #
 # Distributed under terms of the MIT license.
 #
+source "`pwd`/.env"
+export TZ="Asia/Shanghai"
 
 set -e
 set -x
 
 export GOPATH=$GOPATH:`pwd`
 
-if [ "$GOOS" = "linux" ]; then
-    go build -ldflags '-extldflags "-static"' -o release/{{cookiecutter.project_slug}} {{cookiecutter.project_slug}}
+if [ -x "$(command -v git)" ]; then
+	# GITCOMMIT=$(git rev-parse --short HEAD)
+	GITCOMMIT=$(git rev-parse HEAD)
 else
-    go build -o release/{{cookiecutter.project_slug}} {{cookiecutter.project_slug}}
+	GITCOMMIT=$(date '+%Y%m%d%H%M%S')
+fi
+
+
+if [ "$GOOS" = "linux" ]; then
+    go build -ldflags '-extldflags "-static"' -ldflags "-X wgo.AppVersion=${version} -X wgo.GitCommit=${GITCOMMIT} -X wgo.AppLevel=${level} -X 'wgo.BuildTime=`date -R`'" -o release/{{cookiecutter.project_slug}} {{cookiecutter.project_slug}}
+else
+    go build -ldflags "-X wgo.AppVersion=${version} -X wgo.GitCommit=${GITCOMMIT} -X wgo.AppLevel=${level} -X 'wgo.BuildTime=`date -R`'" -o release/{{cookiecutter.project_slug}} {{cookiecutter.project_slug}}
 fi
